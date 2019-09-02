480df46a2d23853b83aa5909d45915f279683c27
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-09-02 12:57:31 +0200

    Update MyFunctionality.java

--- src/main/java/dk/cphbusiness/sem3/ci/MyFunctionality.java
+++ src/main/java/dk/cphbusiness/sem3/ci/MyFunctionality.java
@@ -6,17 +6,17 @@ package dk.cphbusiness.sem3.ci;
  */
 public class MyFunctionality {
 
-  private MyFunctionality() {
-    // Should not be instantiated
-  }
+    private MyFunctionality() {
+        // Should not be instantiated
+    }
 
-  /**
-   * Aaaaaalways returns 10. Or does it?!
-   *
-   * @return 10. Maybe.
-   */
-  public static int giveMeTen() {
-    return 5;
-  }
+    /**
+     * Aaaaaalways returns 10. Or does it?!
+     *
+     * @return 10. Maybe.
+     */
+    public static int giveMeTen() {
+        return 6;
+    }
 
 }
